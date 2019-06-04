9831c80001dcd863f3d5c1a9806e34a0379bdc88
Alexandr Serbin <serbin@rarus.ru>
2019-06-04 09:34:01 +0300

    test cases started

---
+++ tests/Evaluating/PassControllerTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Tests\Evaluating;
+
+use App\Http\Controllers\User\Homework\PassController;
+use PHPUnit\Framework\TestCase;
+
+class PassControllerTest extends TestCase
+{
+
+    public function testSetMark()
+    {
+
+    }
+}
